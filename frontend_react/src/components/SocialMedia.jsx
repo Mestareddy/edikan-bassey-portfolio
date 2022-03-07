234cdf1a4e5ffabcfd25d70c287a997d27057d7d
Mestareddy <60517569+Mestareddy@users.noreply.github.com>
2022-03-07 13:21:06 +0100

    socials

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -7,15 +7,19 @@ const SocialMedia = () => {
     <div className='app__social'>
       <div>
         <BsTwitter />
+        <a href='https://twitter.com/mestareddy'></a>
       </div>
       <div>
         <FaFacebookF />
+        <a href='https://www.facebook.com/Edikanibassey/'></a>
       </div>
       <div>
         <BsInstagram />
+        <a href='https://www.instagram.com/mredikan/'></a>
       </div>
       <div>
         <BsGithub />
+        <a href='https://github.com/Mestareddy'></a>
       </div>
     </div>
   );
