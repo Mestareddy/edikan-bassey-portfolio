177e3d5d1f8d720155f366059eddcd12fdf493b1
mac <edikan.i.bassey@gmail.com>
2023-05-11 22:08:44 +0100

    update

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -21,17 +21,18 @@ const Skills = () => {
       setSkills(data);
     });
   }, []);
+
   return (
     <>
       <h2 className='head-text'>Skills & Experience</h2>
       <div className='app__skills-container'>
         <motion.div className='app__skills-list'>
-          {skills?.map((skill) => (
+          {skills?.map((skill, index) => (
             <motion.div
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className='app__skills-item app__flex'
-              key={skill.name}
+              key={index}
             >
               <div
                 className='app__flex'
@@ -44,21 +45,20 @@ const Skills = () => {
           ))}
         </motion.div>
         <motion.div className='app__skills-exp'>
-          {experience?.map((experience) => (
-            <motion.div className='app__skills-exp-item' key={experience.year}>
+          {experience?.map((experience, index) => (
+            <motion.div className='app__skills-exp-item' key={index}>
               <div className='app__skills-exp-year'>
                 <p className='bold-text'>{experience.year}</p>
               </div>
               <motion.div className='app__skills-exp-works'>
-                {experience.works.map((work) => (
-                  <>
+                {experience.works.map((work, index) => (
+                  <div key={index}>
                     <motion.div
                       whileInView={{ opacity: [0, 1] }}
                       transition={{ duration: 0.5 }}
                       className='app__skills-exp-work app__flex'
                       data-tip
                       data-for={work.name}
-                      key={work.name}
                     >
                       <h4 className='bold-text'>{work.name}</h4>
                       <p className='p-text'>{work.company}</p>
@@ -71,7 +71,7 @@ const Skills = () => {
                     >
                       {work.desc}
                     </ReactTooltip>
-                  </>
+                  </div>
                 ))}
               </motion.div>
             </motion.div>
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 
@@ -12,7 +12,7 @@ const Work = () => {
   const [works, setWorks] = useState([]);
   const [filterWork, setFilterWork] = useState([]);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const query = '*[_type == "works"]';
 
     client.fetch(query).then((data) => {
@@ -28,24 +28,34 @@ const Work = () => {
     setTimeout(() => {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
-      if (item === "All") {
-        setFilterWork(works);
+      if (item !== "All") {
+        // console.log("initialised", item);
+        // console.log("works.tags", works);
+        // if (works.tags.includes(item)) {
+        //   console.log("gotten");
+        // }
+        setFilterWork(works.filter((work) => work?.tags.includes(item)));
       } else {
-        setFilterWork(works.filter((work) => work.tags.includes(item)));
+        console.log("works", works);
+        setFilterWork(works);
       }
     }, 500);
   };
+  // console.log(filterWork);
+
   return (
     <>
       <h2 className='head-text'>
         My Creative <span>Portfolio</span> Section
       </h2>
       <div className='app__work-filter'>
-        {["UI/UX", "Node JS", "Next JS", "React JS", "All"].map(
+        {["UI/UX", "Node JS", "Next JS", "React JS", "All"]?.map(
           (item, index) => (
             <div
               key={index}
-              onClick={() => handleWorkFilter(item)}
+              onClick={() => {
+                handleWorkFilter(item);
+              }}
               className={`app__work-filter-item app__flex p-text ${
                 activeFilter === item ? "item-active" : ""
               }`}
@@ -60,10 +70,13 @@ const Work = () => {
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className='app__work-portfolio'
       >
-        {filterWork.map((work, index) => (
+        {filterWork?.map((work, index) => (
           <div className='app__work-item app__flex' key={index}>
             <div className='app__work-img app__flex'>
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
+              <img
+                src={work.imgUrl ? urlFor(work.imgUrl) : ""}
+                alt={work?.name}
+              />
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{
@@ -73,7 +86,7 @@ const Work = () => {
                 }}
                 className='app__work-hover app__flex'
               >
-                <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                <a href={work?.projectLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -87,7 +100,7 @@ const Work = () => {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                <a href={work?.codeLink} target='_blank' rel='noreferrer'>
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -104,13 +117,13 @@ const Work = () => {
               </motion.div>
             </div>
             <div className='app__work-content app__flex'>
-              <h4 className='bold-text'>{work.title}</h4>
+              <h4 className='bold-text'>{work?.title}</h4>
               <p className='p-text' style={{ marginTop: 10 }}>
-                {work.description}
+                {work?.description}
               </p>
 
               <div className='app__work-tag app__flex'>
-                <p className='p-text'>{work.tags[0]}</p>
+                <p className='p-text'>{work?.tags ? work?.tags[0] : ""}</p>
               </div>
             </div>
           </div>
